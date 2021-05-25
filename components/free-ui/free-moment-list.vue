f1926bc3b66b3fbb4cb068353428c77305c78fa2
伤心的瘦子 <54710109+C4az6@users.noreply.github.com>
2021-05-26 02:39:39 +0800

    92-朋友圈页面-朋友圈列表组件-评论&点赞开发

--- components/free-ui/free-moment-list.vue
+++ components/free-ui/free-moment-list.vue
@@ -9,19 +9,48 @@
 			<text class="text-hover-primary font-md mb-1">{{item.username}}</text>
 			<!-- 内容 -->
 			<text class="font-md text-dark mb-1">{{item.context}}</text>
-				<view class="py-2 flex flex-wrap" v-if="item.image.length">
-					<block v-for="(image, imageIndex) in item.image">
-						<!-- 单图 -->
-						<free-image v-if="item.image.length === 1" :src="image.src" mode="aspectFill" class="mb-2" @click="previewImage(image.src)"></free-image>
-						<!-- 多图 -->
-						<image v-else :src="image.src" mode="aspectFill" style="width: 180rpx;height: 180rpx;" :key="index" class="mr-2 mb-2 rounded" @click="previewImage(image.src)">
-						</image>
-					</block>
-				</view>
-			<!-- 时间和操作按钮 -->
+			<view class="py-2 flex flex-wrap" v-if="item.image.length">
+				<block v-for="(image, imageIndex) in item.image">
+					<!-- 单图 -->
+					<free-image v-if="item.image.length === 1" :src="image.src" mode="aspectFill" class="mb-2" @click="previewImage(image.src)"></free-image>
+					<!-- 多图 -->
+					<image v-else :src="image.src" mode="aspectFill" style="width: 180rpx;height: 180rpx;" :key="index" class="mr-2 mb-2 rounded" @click="previewImage(image.src)">
+					</image>
+				</block>
+			</view>
+			
+			<!-- 视频部分 -->
+			<view class="py-2" v-if="item.video">
+				<video :src="item.video.src" :poster="item.video.poster" controls loop style="height: 300rpx;width: 500rpx;"></video>
+			</view>
+			
+			<!-- 时间 | 操作按钮 -->
 			<view class="flex justify-between align-center">
 				<text class="text-light-muted font-sm">{{item.create_time | timeFormat}}</text>
 				<text class="text-hover-primary iconfont font p-1">{{'\ue6fd'}}</text>
+			</view>
+			
+			<!-- 点赞 | 评论部分 -->
+			<view class="bg-light" v-if="item.supports.length || item.comments.length">
+				<!-- 点赞部分 -->
+				<view class="flex align-start border-bottom p-2" v-if="item.supports.length">
+					<text class="iconfont font text-hover-primary">&#xe637;</text>
+					<view class="flex flex-wrap flex-1">
+						<text class="font text-hover-primary ml-2" v-for="s in item.supports">{{s.username}}</text>
+					</view>
+				</view>
+				
+				
+				<!-- 评论部分 -->
+				<view class="flex align-start mt-2 p-2" v-if="item.comments.length">
+					<text class="iconfont font-lg text-hover-primary">&#xe64e;</text>
+					<view class="flex-1 ml-2">
+						<view class="flex mb-2 pb-2 border-bottom" v-for="c in item.comments">
+							<text class="font text-hover-primary">{{c.username}} :</text>
+							<text class="font text-dark ml-2 flex-1">{{c.content}}</text>
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
